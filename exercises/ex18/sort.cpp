46e15f55aa642e96170c66b59d70304ff88ec95c
Jamie <jstelni1@jh.edu>
2021-12-14 21:39:59 -0500

    ex1 works

--- exercises/ex18/sort.cpp
+++ exercises/ex18/sort.cpp
@@ -13,11 +13,35 @@ int main( void )
 
   // PART 2 TO DO: read in the count of random numbers from the standard input.
 
+  std::cin >> count;
+  
   // PART 3 TO DO: Make the "vec" vector store "count" random values
 
-  sort( &vec );
+  vec.resize(count);
 
-  for( size_t i=1 ; i<vec.size() ; i++ ) assert( vec[i-1]<=vec[i] );
+  for(size_t i = 0; i < count; i++)
+  {
+    vec[i] = rand();
+  }
+
+  for(size_t i = 0; i < vec.size(); i++)
+  {
+    std::cout << vec[i] << " ";
+  }
+  std:: cout << std::endl;
+
+  sort(&vec);
+
+  for(size_t i = 1; i < vec.size(); i++)
+  {
+      assert(vec[i-1] <= vec[i]);
+  }
+
+  for(size_t i = 0; i < vec.size(); i++)
+  {
+    std::cout << vec[i] << " ";
+  }
+  std:: cout << std::endl;
 
   return 0;
 }
@@ -25,5 +49,43 @@ int main( void )
 void sort( std::vector< int > *values )
 {
   // PART 5 TO DO: Define the sort function
+  if(values->size() > 1)
+  {
+    int mid = values->size() / 2;
+    std::vector<int> left(mid);
+    std::vector<int> right(values->size() - mid);
+    for(size_t i = 0; i < mid; i++)
+    {
+      left[i] = (*values)[i];
+    }
+    for(size_t i = mid; i < values->size(); i++)
+    {
+      right[i - mid] = (*values)[i];
+    }
+
+    sort(&left);
+    sort(&right);
+
+    size_t i = 0, j = 0, k = 0;
+    while(i < left.size() || j < right.size())
+    {
+      if(left.size() <= i)
+      {
+        (*values)[k++] = right[j++];
+      }
+      else if(right.size() <= j)
+      {
+        (*values)[k++] = left[i++];
+      }
+      else if(left[i] < right[j])
+      {
+        (*values)[k++] = left[i++];
+      }
+      else
+      {
+        (*values)[k++] = right[j++];
+      }
+    }
+  }
 }
 
